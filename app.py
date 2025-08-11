62a430c82084370265700fa8338e53a16f7a0643
Perugu Koti Sai Prakash <kotisaiprakash7@gmail.com>
2025-08-11 18:40:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -543,6 +543,6 @@ def update_goal_progress():
     return jsonify({'success': True})
 
 if __name__ == "__main__":
+    
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=port,debug=True)
